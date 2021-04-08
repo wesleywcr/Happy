02c5b4f7b7184e2e3e20e015b44a3000f1b5ed47
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2021-04-08 15:05:49 -0300

    Update database

--- server/src/views/images-views.ts
+++ server/src/views/images-views.ts
@@ -5,7 +5,7 @@ export default {
   render(image: Image) {
     return {
       id: image.id,
-      url: `http://10.0.0.102:3333/uploads/${image.path}`,
+      url: `http://localhost:3333/uploads/${image.path}`,
 
     };
   },
